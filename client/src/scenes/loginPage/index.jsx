5cedd1bab0fdadc5c4227b4d65c52209c5e1e7b9
eshakumar <e3kumar@uwaterloo.ca>
2023-06-21 16:49:42 -0400

    Add login page message

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -28,12 +28,16 @@ const LoginPage = () => {
                 borderRadius="1.5rem"
                 backgroundColor={theme.palette.background.alt}
             >
-                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                <Typography fontWeight="500" variant="h2" sx={{ mb: "1.5rem" }}>
                     Welcome to Sociopedia!
                 </Typography>
                 <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-                    !!Caution!! If you are creating an account, please do not use any real passwords.
+                    Caution: If you are creating an account, please do not use any real passwords. This app is still a work-in-progress.
                 </Typography>
+                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                    The first login may take up to a minute due to slow on-demand server hosting.
+                </Typography>
+                <p><strong>Demo Credentials</strong><br></br>Username: demo@demo.com<br></br> Password: password123!</p>
                 <Form />
             </Box>
         </Box>
